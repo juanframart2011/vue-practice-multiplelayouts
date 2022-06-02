129fd915f681373796196c548f0716df536e346f
rogelio.franco <rogelio.franco@stratplus.net>
2022-06-01 23:13:19 -0500

    finish project

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,12 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <component :is="layout"></component>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  import LoginLayout from './components/layouts/Login'
+  import PrincipalLayout from './components/layouts/Principal'
+  import {mapState} from 'vuex'
+  export default {
+    components: {LoginLayout, PrincipalLayout},
+    computed: mapState(['layout'])
   }
-}
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,21 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
+import {store} from './store' // importamos el modulo de store
 
-createApp(App).mount('#app')
+Vue.use(Vuetify)
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  store, // registramos el store
+  router,
+  components: { App },
+  template: '<App/>'
+})
